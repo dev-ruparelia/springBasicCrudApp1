f3bd1ceff37a12e946f5d1669d0294887748f994
EZDI\devesh.r <devesh.r@ezdi.us>
2018-04-26 15:21:48 +0530

    Initial commit

---
+++ src/main/java/com/example/springCrudApp1/SpringCrudApp1Application.java
@@ -0,0 +1,12 @@
+package com.example.springCrudApp1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCrudApp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCrudApp1Application.class, args);
+	}
+}
