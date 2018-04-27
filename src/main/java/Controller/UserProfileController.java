740d048536b2a325bd4392781f29e90c4d2c9bdf
dev-ruparelia <devesh_ruparelia@yahoo.com>
2018-04-27 11:40:27 +0530

    small change

--- src/main/java/Controller/UserProfileController.java
+++ src/main/java/Controller/UserProfileController.java
@@ -1,7 +1,14 @@
 package Controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Created by EZDI\devesh.r on 27/4/18.
  */
+@RestController
+@RequestMapping("/user")
 public class UserProfileController {
+    // add controller methods here
 }
